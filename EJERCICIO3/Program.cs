b03bc5e551b4b4d42aac21f03baf9e617dc8db8d
alexi <alexi@ALEXANDER>
2023-08-27 14:39:54 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese las tres notas parciales:");
+int CP1 = int.Parse(Console.ReadLine());
+int CP2 = int.Parse(Console.ReadLine());
+int CP3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su nota de examen final");
+int CEF = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su nota de trabajo final");
+int CTF = int.Parse(Console.ReadLine());
+
+double NF = 0.55 * (CP1 + CP2 + CP3) / 3 + 0.30 * CEF + 0.15 * CTF;
+
+Console.WriteLine("Su nota final es " + NF);
